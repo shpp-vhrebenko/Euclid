1cf422fb19b605b0e2ab7269ce4872717936dd24
Vladymyr Hrebenko <volgan16@gmail.com>
2022-02-10 14:38:04 +0200

    fix header menu adaptive

--- js/main.js
+++ js/main.js
@@ -1,63 +1,92 @@
-document.addEventListener('DOMContentLoaded', function() {  
-  const navButton = document.querySelector(".header__hamburger");
-  const navList = document.querySelector(".header__bottom");
+document.addEventListener('DOMContentLoaded', function() {   // отлавливаю событие загрузки страници https://developer.mozilla.org/ru/docs/Web/API/Window/DOMContentLoaded_event
+  const navButton = document.querySelector(".header__hamburger"); // получаю елемент navButton по селектору для дальнейшего его использования
+  const navList = document.querySelector(".header__panel"); // получаю елемент navList по селектору для дальнейшего его использования
 
-  if(navButton) {
-    navButton.addEventListener("click", function(e) {
-      document.body.classList.toggle('lock');
-      navButton.classList.toggle("header__hamburger--active");
-      navList.classList.toggle("header__bottom--active");
+  if(navButton) { // проверка если елемент существует то ...
+    navButton.addEventListener("click", function(e) { // отлавливаем событие нажатия на navButton
+      document.body.classList.toggle('lock'); // ставим переключатель класса для елемента body
+      navButton.classList.toggle("header__hamburger--active"); // ставим переключатель класса для елемента navButton
+      navList.classList.toggle("header__panel--active"); // ставим переключатель класса для елемента navList
     })
   }
 
-  const menuLinks = document.querySelectorAll('.nav-header__link[data-goto]');  
-  if(menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-      menuLink.addEventListener("click", onMenuLinkClick);  
-    });
+  const menuLinks = document.querySelectorAll('.nav-header__link[data-goto]');  // получаем список елементов по селектору (список типа NodeList)
+  if(menuLinks.length > 0) { // если список не пустой то...
+    menuLinks.forEach(menuLink => { // перебираем елементы списка
+      menuLink.addEventListener("click", onMenuLinkClick);  // для каждого елемента списка отлавливаем событие нажатия на него, как результат выполнение функции onMenuLinkClick
+    });   
+  }
 
-    function onMenuLinkClick(e) {
-      const menuLink = e.target;
-      console.log(menuLink.dataset.goto);
-      console.log(document.querySelector(menuLink.dataset.goto));
-      if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
-        const gotoBlock = document.querySelector(menuLink.dataset.goto);
-        console.log(gotoBlock);
-        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector("header").offsetHeight;
 
-        if(navButton.classList.contains('header__hamburger--active')) {
-          document.body.classList.remove('lock');
-          navButton.classList.remove("header__hamburger--active");
-          navList.classList.remove("header__bottom--active");
-        }
+  /**
+     * Получает елемент,
+     * проверяет наличие у него свойства data со значение goto.
+     * Прокручивает страницу до того елемента на который сылается данный елемент.
+     *
+     * @param {Element} e .    
+     */
+  function onMenuLinkClick(e) {  
+    const menuLink = e.target;      
+    if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);        
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector("header").offsetHeight;
 
-        window.scrollTo({
-          top: gotoBlockValue,
-          behavior: "smooth"
-        });
-        e.preventDefault();
+      if(navButton.classList.contains('header__hamburger--active')) {
+        document.body.classList.remove('lock');
+        navButton.classList.remove("header__hamburger--active");
+        navList.classList.remove("header__panel--active");
       }
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
     }
   }
 
 
-  /* SPOLERS */
+  /* ACCORDIONS */
 
-  const accordions = document.querySelectorAll('.FAQ-list__item');
+  const accordions = document.querySelectorAll('.FAQ-list__item');  // получаем список елементов по селектору (список типа NodeList)
 
-  if(accordions.length > 0) {
-    for(item of accordions) {
-      item.addEventListener('click', function() {
-        if(this.classList.contains('active')) {
-          this.classList.remove('active');
-        } else {
-          for(el of accordions) {
-            el.classList.remove('active');
+  if(accordions.length > 0) { // если список не пустой то...
+    for(item of accordions) { // перебираем елементы списка
+      item.addEventListener('click', function() { // для каждого елемента списка отлавливаем событие нажатия на него, при котором ...
+        if(this.classList.contains('active')) {  // если елемент содержит класс 'active' то ...
+          this.classList.remove('active');       // удаляем у него класс 'active'
+        } else {                                 // иначе
+          for(el of accordions) {                // перебираем елементы списка
+            el.classList.remove('active');       // удаляем у всех елементов класс 'active'
           }
-          this.classList.add('active');
+          this.classList.add('active');          // добавляем данному елементу класс 'active'
         }
       })
     }
   }
 
+
+  /* TABS */
+
+  const tabs = document.querySelectorAll('.work-nav__button');  // получаем список елементов по селектору (список типа NodeList)
+  if(tabs.length > 0) { // если список не пустой то...
+    tabs.forEach(function(tabsBtn){ // перебираем елементы списка      
+      tabsBtn.addEventListener('click', function(event){ // для каждого елемента списка отлавливаем событие нажатия на него, при котором ...
+        const path = event.currentTarget.dataset.path; // получаем значение атрибута data со значением path
+  
+        const tabsContent = document.querySelectorAll('.how-work__content'); // получаем список елементов по селектору (список типа NodeList)
+        if(tabsContent.length > 0) { // если список не пустой то...
+          tabsContent.forEach(function(tabContent) { // перебираем елементы списка 
+            tabContent.classList.remove('how-work__content--active'); // для каждого елемента списка убираем у него класс который отвечает за его активное состояние.
+          })
+        }
+        
+        if(document.querySelector(`[data-target="${path}"]`)) { // если есть елемент с определёным значением атрибута data-target то...
+          document.querySelector(`[data-target="${path}"]`).classList.add('how-work__content--active') // добавляем етому елементу определенный класс
+        }
+
+      }); 
+      
+    });
+  }
 });
